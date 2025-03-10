f9f91ba8edaae9c40fbd2b6e1103ba672f940c67
luumartinez <luuumartinez13@gmail.com>
2025-03-10 20:52:06 -0300

    configuracion login y logout

--- src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
@@ -1,6 +1,7 @@
 package com.egg.biblioteca.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +25,17 @@ public class PortalControlador {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(required = false) String error, ModelMap modelo) {
+        if(error != null){
+            modelo.put("error", "Usuario o Contraseña inválidos!");
+        }
         return "login.html";
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER, ROLE_ADMIN')")
+    @GetMapping("/inicio")
+    public String inicio() {
+        return "inicio.html";
     }
 
     @GetMapping("/registrar")
